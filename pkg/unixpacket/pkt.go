9f345e42791d2cb75c963f751da952de313f47a2
Ilya Gavrilov <gilyav@gmail.com>
2024-05-10 08:33:23 +0200

    add packet direction

--- pkg/unixpacket/pkt.go
+++ pkg/unixpacket/pkt.go
@@ -4,12 +4,20 @@ import (
 	"unsafe"
 )
 
+type PacketDirection uint8
+
+const (
+	PacketRecived PacketDirection = 0
+	PacketSent    PacketDirection = 1
+)
+
 const PacketHeaderSize = int(unsafe.Sizeof(PacketUnixSocketHeader{}))
 
 type PacketUnixSocketHeader struct {
 	PacketCounter uint64
 	Timestamp     uint64
 	CgroupID      uint64
+	Direction     PacketDirection
 }
 
 type PacketUnixSocket []byte
